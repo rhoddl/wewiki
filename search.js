57f4164918f9b40d88ef6baefd6dc4b91803d01f
unknown <senkim9377@gmail.com>
2020-04-02 17:08:10 +0900

    first

---
+++ search.js
@@ -0,0 +1,34 @@
+const searchform = document.querySelector('.searchForm');
+const searchbox = document.querySelector('.searchBox');
+
+searchform.innerHTML
+
+X= [];
+
+function toWiki(){
+    const inputValue = searchbox.value;
+    wikiUrl = `https://namu.wiki/Search?q=${inputValue}`;
+    location.href = wikiUrl;
+}
+
+function noWord(){
+    const inputValue = searchbox.value;
+    if(inputValue === ''){
+        alert( "검색어를 입력하세요.");
+    } else{
+        toWiki();
+    }
+}
+
+searchform.addEventListener('submit', noWord);
+searchform.addEventListener('submit', handleDefault = (event) =>{
+    event.preventDefault();
+});
+
+
+
+function init(){
+}
+
+
+init();
